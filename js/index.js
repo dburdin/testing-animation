07756d02e91fa08cee1489cc8c97e363c699d04e
Dmytrii Bevziuk <113050865+dburdin@users.noreply.github.com>
2023-06-10 12:20:39 +0300

    adding button to say goodbye to astronaut

---
+++ js/index.js
@@ -0,0 +1,19 @@
+const button = document.querySelector(".goodbye-btn");
+
+const animatedElement = document.querySelector(".use-tag-2");
+const animatedShadow = document.querySelector(".use-tag-2-shadow");
+const astroContainer = document.querySelector(".astro-container");
+
+button.addEventListener("click", handleClick);
+
+function handleClick() {
+  animatedElement.classList.add("use-tag-2--active");
+  animatedShadow.classList.add("use-tag-2-shadow--active");
+
+  setTimeout(() => {
+    astroContainer.style.opacity = "0";
+  }, 2000);
+  setTimeout(() => {
+    button.style.opacity = "0";
+  }, 2000);
+}
